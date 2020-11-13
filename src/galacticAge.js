695a01785df669c0aa562beba78dd2f25d696582
Pugsly99 <rylandadams@yahoo.com>
2020-11-13 09:57:16 -0800

    first planet function in person class

--- src/galacticAge.js
+++ src/galacticAge.js
@@ -3,6 +3,40 @@ export default class Person {
   constructor(name, age) {
     this.name = name;
     this.age = age;
+    this.lifeExpect = 78.5;
+    this.mercury = 0;
+    this.venus = 0;
+    this.mars = 0;
+    this.jupiter = 0;
   }
-  
+
+  mercuryAge(){
+    let mercuryYear = .24;
+    let yearsLeft = 0;
+    let planetAge = this.age % mercuryYear;
+    let planetLife = this.lifeExpect % mercuryYear;
+
+    if(planetAge < planetLife) {
+      this.mercury = planetAge;
+      yearsLeft = planetLife - planetAge
+      console.log("Your ${planetAge} years old on Mercury and you have ${yearsLeft} years left!")
+
+    } else if(planetAge > planetLife) {
+      this.mercury = planetAge;
+      yearsLeft = planetAge - planetLife
+      console.log("Your ${planetAge} years old on Mercury and thats ${yearsLeft} years more than your expected to live!")
+    }
+
+  }
+  venusAge(){
+
+  }
+
+  marsAge(){
+
+  }
+
+  jupiterAge(){
+
+  }
 }
