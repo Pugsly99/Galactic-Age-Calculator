1b14d42159b5695a66ae10a048b1d7c219e84b96
Pugsly99 <rylandadams@yahoo.com>
2020-11-13 10:14:30 -0800

    wrote first test person

---
+++ __tests__/galacticAge.js
@@ -0,0 +1,14 @@
+import Person from "./../src/galacticAge.js";
+
+describe("Person class and all its methods", () => {
+  let person;
+
+  beforeEach(() => {
+    person = new Person ("Ryland", 21);
+  });
+
+  test("create new person object with arguments", () => {
+    expect(person.name).toEqual("Ryland");
+    expect(person.age).toEqual(22);
+  })
+})
