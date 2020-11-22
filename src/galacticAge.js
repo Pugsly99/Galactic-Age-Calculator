95b924bd341c1db96a251e753a6c9ffe0ab21408
Pugsly99 <rylandadams@yahoo.com>
2020-11-22 13:46:17 -0800

    removed this.planetsname from the if else statements

--- src/galacticAge.js
+++ src/galacticAge.js
@@ -25,11 +25,9 @@ export default class Person {
     let planetLife = this.lifeExpect / mercuryYear;
     
     if(this.mercury < planetLife) {
-      this.mercury 
       this.mercuryYearleft = planetLife - this.mercury
       console.log("Your ${this.mercury} years old on Mercury and you have ${this.mercuryYearleft} years left!")
     } else if(this.mercury > planetLife) {
-      this.mercury 
       this.mercuryYearleft = this.mercury - planetLife
       console.log("Your ${this.mercury} years old on Mercury and thats ${this.mercuryYearleft} years more than your expected to live!")
     }
@@ -41,12 +39,10 @@ export default class Person {
     let planetLife = this.lifeExpect / venusYear;
     
     if(this.venus < planetLife) {
-      this.venus 
       this.venusYearleft = planetLife - this.venus
       outcome = ("Your ${this.venus} years old on Mercury and you have ${this.venusYearleft} years left!")
       return outcome
     } else if(this.venus > planetLife) {
-      this.venus 
       this.venusYearleft = this.venus - planetLife
       outcome = ("Your ${this.venus} years old on Mercury and thats ${this.venusYearleft} years more than your expected to live!")
       return outcome
@@ -59,11 +55,9 @@ export default class Person {
     let planetLife = this.lifeExpect / marsYear;
 
     if(this.mars < planetLife) {
-      this.mars 
       this.marsYearleft = planetLife - this.mars
       console.log("Your ${this.mars} years old on Mercury and you have ${this.marsYearleft} years left!")
     } else if(this.mars > planetLife) {
-      this.mars 
       this.marsYearleft = this.mars - planetLife
       console.log("Your ${this.mars} years old on Mercury and thats ${this.marsYearleft} years more than your expected to live!")
     }
@@ -75,11 +69,9 @@ export default class Person {
     let planetLife = this.lifeExpect / jupiterYear;
     
     if(this.jupiter < planetLife) {
-      this.jupiter 
       this.jupiterYearleft = planetLife - this.jupiter
       console.log("Your ${this.mars} years old on Mercury and you have ${this.jupiterYearleft} years left!")
     } else if(this.jupiter > planetLife) {
-      this.jupiter 
       this.jupiterYearleft = this.jupiter - planetLife
       console.log("Your ${this.jupiter} years old on Mercury and thats ${this.jupiterYearleft} years more than your expected to live!")
     }
