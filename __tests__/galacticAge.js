e6ad5b57a0d611b7e722881c7fa0b36db4425fde
Pugsly99 <rylandadams@yahoo.com>
2020-11-22 15:41:44 -0800

    splitting up of all the tests into smaller test

--- __tests__/galacticAge.js
+++ __tests__/galacticAge.js
@@ -22,48 +22,80 @@ describe("Person class and all its methods", () => {
   test("determins me age on mercury", () => {
     me.mercuryAge();
     expect (me.mercury).toEqual(87.5);
+  });
+
+  test("determins me years left on mercury", () => {
+    me.mercuryAge();
     expect(me.mercuryYearleft).toEqual(239.6);
   });
 
   test("determins dad age on mercury", () => {
     dad.mercuryAge();
     expect (dad.mercury).toEqual(833.3);
+  });
+
+  test("determins dad years left on mercury", () => {
+    dad.mercuryAge();
     expect(dad.mercuryYearleft).toEqual(506.2);
   });
 
   test("determins age on venus", () => {
     me.venusAge();
     expect(me.venus).toEqual(33.9);
+  });
+
+  test("determins me years left on venus", () => {
+    me.venusAge();
     expect(me.venusYearleft).toEqual(92.7);
   });
 
   test("determins dad age on venus", () => {
     dad.venusAge();
     expect (dad.venus).toEqual(322.6);
+  });
+
+  test("determins dad years left on venus", () => {
+    dad.venusAge();
     expect(dad.venusYearleft).toEqual(196);
   });
 
-  test("determins age on mars", () => {
+  test("determinsme age on mars", () => {
     me.marsAge();
     expect(me.mars).toEqual(11.2);
+  });
+
+  test("determins me years left on mars", () => {
+    me.marsAge();
     expect(me.marsYearleft).toEqual(30.6);
   });
   
   test("determins dad age on mars", () => {
     dad.marsAge();
     expect (dad.mars).toEqual(106.4);
+  });
+
+  test("determins dad years left on mars", () => {
+    dad.marsAge();
     expect(dad.marsYearleft).toEqual(64.6);
   });
 
-  test("determins age on jupiter", () => {
+  test("determins me age on jupiter", () => {
     me.jupiterAge();
     expect(me.jupiter).toEqual(1.8);
+  });
+
+  test("determins me years left on jupiter", () => {
+    me.jupiterAge();
     expect(me.jupiterYearleft).toEqual(4.8);
   });
 
   test("determins dad age on jupiter", () => {
     dad.jupiterAge();
     expect (dad.jupiter).toEqual(16.9);
+  });
+
+  test("determins dads years left on jupiter", () => {
+    dad.jupiterAge();
     expect(dad.jupiterYearleft).toEqual(10.3);
   });
 })
