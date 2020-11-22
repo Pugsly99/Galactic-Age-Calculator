84f1988dfbea64948a696721ada700f0c8aa86a4
Pugsly99 <rylandadams@yahoo.com>
2020-11-22 15:20:47 -0800

    make up person to allow for jest to cover the other side of the if else statements

--- __tests__/galacticAge.js
+++ __tests__/galacticAge.js
@@ -6,7 +6,7 @@ describe("Person class and all its methods", () => {
 
   beforeEach(() => {
     me = new Person ("Ryland", 21);
-    dad = new Person ("Chad", 103);
+    dad = new Person ("Chad", 200);
   });
 
   test("create new me object with arguments", () => {
@@ -16,7 +16,7 @@ describe("Person class and all its methods", () => {
 
   test("create new dad object with arguments", () => {
     expect(dad.name).toEqual("Chad");
-    expect(dad.age).toEqual(103);
+    expect(dad.age).toEqual(200);
   });
   
   test("determins me age on mercury", () => {
@@ -27,8 +27,8 @@ describe("Person class and all its methods", () => {
 
   test("determins dad age on mercury", () => {
     dad.mercuryAge();
-    expect (dad.mercury).toEqual(87.5);
-    expect(dad.mercuryYearleft).toEqual(239.6);
+    expect (dad.mercury).toEqual(833.3);
+    expect(dad.mercuryYearleft).toEqual(506.2);
   });
 
   test("determins age on venus", () => {
@@ -37,15 +37,33 @@ describe("Person class and all its methods", () => {
     expect(me.venusYearleft).toEqual(92.7);
   });
 
+  test("determins dad age on venus", () => {
+    dad.venusAge();
+    expect (dad.venus).toEqual(322.6);
+    expect(dad.venusYearleft).toEqual(196);
+  });
+
   test("determins age on mars", () => {
     me.marsAge();
     expect(me.mars).toEqual(11.2);
     expect(me.marsYearleft).toEqual(30.6);
+  });
+  
+  test("determins dad age on mars", () => {
+    dad.marsAge();
+    expect (dad.mars).toEqual(106.4);
+    expect(dad.marsYearleft).toEqual(64.6);
   });
 
   test("determins age on jupiter", () => {
     me.jupiterAge();
     expect(me.jupiter).toEqual(1.8);
     expect(me.jupiterYearleft).toEqual(4.8);
+  });
+
+  test("determins dad age on jupiter", () => {
+    dad.jupiterAge();
+    expect (dad.jupiter).toEqual(16.9);
+    expect(dad.jupiterYearleft).toEqual(10.3);
   });
 })
